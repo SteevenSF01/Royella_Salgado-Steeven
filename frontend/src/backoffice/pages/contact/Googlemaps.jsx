2f96fd80b8793b29d6899931780a5a480d91cd12
SteevenSF01 <salgadosteeven@gmail.com>
2024-06-08 21:40:02 +0200

    fin de la section contact

--- frontend/src/backoffice/pages/contact/Googlemaps.jsx
+++ frontend/src/backoffice/pages/contact/Googlemaps.jsx
@@ -1,12 +1,11 @@
-// src/App.js
 import React from 'react';
 import Searchbar from './Searchbar';
 
 export default function Autocomplete() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-2xl font-bold">Introduisez votre adresse</h1>
+    <div className="h-fit">
+      <header className="">
+        <h1 className="text-2xl font-bold text-center">Introduisez votre adresse</h1>
         <Searchbar />
       </header>
     </div>
--- frontend/src/backoffice/pages/contact/Searchbar.jsx
+++ frontend/src/backoffice/pages/contact/Searchbar.jsx
@@ -1,31 +1,68 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
 export default function Searchbar() {
+    const notifySuccess = () =>
+        toast.success("Data updated successfully", {
+            position: "top-left",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Slide,
+        });
+
+    const notifyError = () =>
+        toast.error("AError occurred during the upload", {
+            position: "top-left",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Slide,
+        });
+
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
-    const [selectedLocation, setSelectedLocation] = useState(null);
     const [data, setData] = useState([]);
+    const [formData, setFormData] = useState({
+        text: "",
+        email: "",
+        numero: "",
+        address: "",
+        latitude: "",
+        longitude: "",
+    });
 
     useEffect(() => {
         const fetchData = async () => {
-            try{
+            try {
                 const res = await axios.get(`/api/backoffice/contact/`);
-                setData(res.data);
-            }catch(error){
+                const donnees = res.data.slice(0, 1);
+                setData(donnees);
+                if (donnees.length > 0) {
+                    setFormData(donnees[0]);
+                }
+            } catch (error) {
                 console.error(error);
             }
         };
         fetchData();
     }, []);
-    console.log(data);
 
     const handleInputChange = async (e) => {
         const value = e.target.value;
         setQuery(value);
 
         if (value.length > 2) {
-            // Ajout du debouncing pour limiter le nombre de requêtes à la fois
             const timeoutId = setTimeout(async () => {
                 const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
                     value
@@ -48,56 +85,191 @@ export default function Searchbar() {
     };
 
     const handleSuggestionClick = (suggestion) => {
-        setSelectedLocation({
-            lat: suggestion.lat,
-            lon: suggestion.lon,
-            displayName: suggestion.display_name,
-        });
         setQuery(suggestion.display_name);
         setSuggestions([]);
+        setFormData({
+            ...formData,
+            address: suggestion.display_name,
+            latitude: suggestion.lat,
+            longitude: suggestion.lon,
+        });
+    };
+
+    const handleFormChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.put(
+                `/api/backoffice/contact/${formData.id}/`,
+                formData
+            );
+            notifySuccess();
+            setTimeout(() => {
+                window.location.reload();
+            }, 2500);
+        } catch (error) {
+            notifyError();
+            console.error("Error updating data:", error);
+        }
     };
 
     return (
-        <div className="flex flex-col items-center mt-10">
-            <input
-                type="text"
-                value={query}
-                onChange={handleInputChange}
-                placeholder="Enter address"
-                className="w-full max-w-md p-2 border border-gray-300 rounded-md"
-            />
-            <ul className="w-full max-w-md mt-2 bg-white border border-gray-300 rounded-md shadow-lg">
-                {suggestions.map((suggestion) => (
-                    <li
-                        key={suggestion.place_id}
-                        className="p-2 cursor-pointer hover:bg-gray-100"
-                        onClick={() => handleSuggestionClick(suggestion)}
-                    >
-                        {suggestion.display_name}
-                    </li>
-                ))}
-            </ul>
-            {selectedLocation && (
-                <div className="w-full max-w-md mt-5">
-                    <div className="w-[400px] h-[400px] bg-red-500 "></div>
-                    <div>
-                        <p>
-                            Latitude: {selectedLocation.lat}, Longitude:{" "}
-                            {selectedLocation.lon} adresse:{" "}
-                            {selectedLocation.displayName}
-                        </p>
-                        <iframe
-                            src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10000!2d${selectedLocation.lon}!3d${selectedLocation.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2s!4v0&zoom=20`}
-                            width="600"
-                            height="450"
-                            style={{ border: 0 }}
-                            allowFullScreen=""
-                            loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"
-                        ></iframe>
-                    </div>
+        <>
+            <section className="h-fit">
+                <div className="flex flex-col items-center py-10">
+                    <input
+                        type="text"
+                        value={query}
+                        onChange={handleInputChange}
+                        placeholder="Enter address"
+                        className="w-full max-w-md p-2 border border-gray-300 rounded-md"
+                    />
+                    <ul className="w-full max-w-md mt-2 bg-white border border-gray-300 rounded-md shadow-lg">
+                        {suggestions.map((suggestion) => (
+                            <li
+                                key={suggestion.place_id}
+                                className="p-2 cursor-pointer hover:bg-gray-100"
+                                onClick={() =>
+                                    handleSuggestionClick(suggestion)
+                                }
+                            >
+                                {suggestion.display_name}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            )}
-        </div>
+                {data &&
+                    data.map((data) => (
+                        <>
+                            <div className="w-[900px] flex justify-center gap-4 h-fit py-10">
+                                <form
+                                    className="w-full max-w-lg mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                                    onSubmit={handleFormSubmit}
+                                    key={data.id}
+                                >
+                                    <div className="mb-4">
+                                        <label
+                                            className="block text-gray-700 text-sm font-bold mb-2"
+                                            htmlFor="text"
+                                        >
+                                            Texte
+                                        </label>
+                                        <textarea
+                                            name="text"
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            rows={5}
+                                            value={formData.text}
+                                            onChange={handleFormChange}
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label
+                                            className="block text-gray-700 text-sm font-bold mb-2"
+                                            htmlFor="email"
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            value={formData.email}
+                                            onChange={handleFormChange}
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label
+                                            className="block text-gray-700 text-sm font-bold mb-2"
+                                            htmlFor="numero"
+                                        >
+                                            Numéro
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="numero"
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            value={formData.numero}
+                                            onChange={handleFormChange}
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label
+                                            className="block text-gray-700 text-sm font-bold mb-2"
+                                            htmlFor="address"
+                                        >
+                                            Adresse
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="address"
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            value={formData.address}
+                                            onChange={handleFormChange}
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label
+                                            className="block text-gray-700 text-sm font-bold mb-2"
+                                            htmlFor="latitude"
+                                        >
+                                            Latitude
+                                        </label>
+                                        <input
+                                            type="number"
+                                            name="latitude"
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            value={formData.latitude}
+                                            onChange={handleFormChange}
+                                        />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label
+                                            className="block text-gray-700 text-sm font-bold mb-2"
+                                            htmlFor="longitude"
+                                        >
+                                            Longitude
+                                        </label>
+                                        <input
+                                            type="number"
+                                            name="longitude"
+                                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            value={formData.longitude}
+                                            onChange={handleFormChange}
+                                        />
+                                    </div>
+                                    <div className="flex items-center justify-between">
+                                        <button
+                                            type="submit"
+                                            className="bg-khaki hover:bg-[rgba(141,115,77,0.9)] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                        >
+                                            Sauvegarder
+                                        </button>
+                                    </div>
+                                </form>
+
+                                <div className="w-[50%] h-[500px]">
+                                    <iframe
+                                        src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10000!2d${data.longitude}!3d${data.latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2s!4v0&zoom=20`}
+                                        width="100%"
+                                        height="100%"
+                                        style={{ border: 0 }}
+                                        allowFullScreen=""
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                    ></iframe>
+                                </div>
+                            </div>
+                        </>
+                    ))}
+                <ToastContainer />
+            </section>
+        </>
     );
 }
